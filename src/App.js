761cb7b0e538845d160303a2cb8dc3ef32bd2279
Ferro <carlosferro@gmail.com>
2019-07-17 08:24:54 +0200

    Added React Router, OrderBook and About

--- src/App.js
+++ src/App.js
@@ -1,96 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import PriceTable from './components/PriceTable'
 import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
-
-
-const subscribeMsg = {
-  "event": "bts:subscribe",
-  "data": {
-    "channel": "order_book_btcusd"
-  }
-};
+import OrderBook from './components/OrderBook'
+import About from './components/About'
+import { Route, Switch, Link, BrowserRouter } from 'react-router-dom'
 
 function App() {
-  // Declare a new state variable, which we'll call "count"
-  const [bids, setBids] = useState([]);
-  const [asks, setAsks] = useState([]);
-
-  useEffect(
-    () => {
-      initWs();
-    },
-    [],
-  );
-
-  let ws;
-  function initWs(pWs) {
-    ws = new WebSocket("wss://ws.bitstamp.net");
-
-    ws.onopen = function () {
-      ws.send(JSON.stringify(subscribeMsg));
-    };
-
-    ws.onmessage = function (evt) {
-      let response = JSON.parse(evt.data);
-      /**
-       * This switch statement handles message logic. It processes data in case of data event
-       * and it reconnects if the server requires.
-       */
-      switch (response.event) {
-        case 'data': {
-          let data = response.data;
-          setBids(data.bids.slice(0, 10))
-          setAsks(data.asks.slice(0, 10))
-          break;
-        }
-        case 'bts:request_reconnect': {
-          initWs();
-          break;
-        }
-        default:
-          break;
-      }
-
-    };
-    ws.onclose = function () {
-      console.log('Websocket connection closed');
-    };
-
-    ws.onclose = function () {
-      console.log('Websocket connection closed');
-      initWs();
-    };
-  }
-
   return (
     <Container fluid>
-      <Navbar className="font-weight-bold" bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="#home">FerroDev</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <NavDropdown title="Portfolio" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Bitstamp</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-
-      <Row className="justify-content-center text-center mt-3">
-        <Col sm="auto">
-          <PriceTable side="bid" priceLevels={bids} />
-        </Col>
-        <Col sm="auto">
-          <PriceTable side="ask" priceLevels={asks} />
-        </Col>
-      </Row>
+      <BrowserRouter>
+        <Navbar className="font-weight-bold" bg="dark" variant="dark" expand="lg">
+          <Navbar.Brand as={Link} to="/">FerroDev</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link as={Link} to="/" >Home</Nav.Link>
+              <NavDropdown title="Portfolio" id="basic-nav-dropdown">
+                <NavDropdown.Item as={Link} to="/bitstamp">Bitstamp</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link as={Link} to="/about" >About</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Switch>
+          <Route exact path="/" />
+          <Route path="/about" component={About} />
+          <Route path="/bitstamp" component={OrderBook} />
+        </Switch>
+      </BrowserRouter>
     </Container>
   );
 }
---
+++ src/components/OrderBook.js
@@ -0,0 +1,77 @@
+import React, {useEffect, useState} from 'react'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import PriceTable from './PriceTable'
+
+const subscribeMsg = {
+    "event": "bts:subscribe",
+    "data": {
+      "channel": "order_book_btcusd"
+    }
+  };
+
+function OrderBook() {
+    // Declare a new state variable, which we'll call "count"
+    const [bids, setBids] = useState([]);
+    const [asks, setAsks] = useState([]);
+
+    useEffect(
+        () => {
+            initWs();
+        },
+        [],
+    );
+
+    let ws;
+    function initWs(pWs) {
+        ws = new WebSocket("wss://ws.bitstamp.net");
+
+        ws.onopen = function () {
+            ws.send(JSON.stringify(subscribeMsg));
+        };
+
+        ws.onmessage = function (evt) {
+            let response = JSON.parse(evt.data);
+            /**
+             * This switch statement handles message logic. It processes data in case of data event
+             * and it reconnects if the server requires.
+             */
+            switch (response.event) {
+                case 'data': {
+                    let data = response.data;
+                    setBids(data.bids.slice(0, 10))
+                    setAsks(data.asks.slice(0, 10))
+                    break;
+                }
+                case 'bts:request_reconnect': {
+                    initWs();
+                    break;
+                }
+                default:
+                    break;
+            }
+
+        };
+        ws.onclose = function () {
+            console.log('Websocket connection closed');
+        };
+
+        ws.onclose = function () {
+            console.log('Websocket connection closed');
+            initWs();
+        };
+    }
+
+    return (
+        <Row className="justify-content-center text-center mt-3">
+            <Col sm="auto">
+                <PriceTable side="bid" priceLevels={bids} />
+            </Col>
+            <Col sm="auto">
+                <PriceTable side="ask" priceLevels={asks} />
+            </Col>
+        </Row>
+    )
+}
+
+export default OrderBook;
