6bc1534810a373316debd981a76c322331dd6c0f
Ferro <carlosferro@gmail.com>
2019-08-15 16:23:52 +0200

    Add express server.

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(favicon('./build/favicon.ico'));
+
+// the __dirname is the current directory from where the script is running
+app.use(express.static('./'));
+app.use(express.static(path.join('./', 'build')));
+app.get('/ping', function (req, res) {
+    return res.send('pong');
+});
+app.get('/*', function (req, res) {
+    res.sendFile(path.join('./', 'build', 'index.html'));
+});
+app.listen(port);
